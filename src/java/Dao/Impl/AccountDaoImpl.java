aee35b31d095ce988d04c148e9526d51df53d351
Pham Hai Minh <119275054+PhamHaiMinh@users.noreply.github.com>
2022-11-29 22:05:47 +0700

    fix update lib

--- src/java/Dao/Impl/AccountDaoImpl.java
+++ src/java/Dao/Impl/AccountDaoImpl.java
@@ -13,7 +13,7 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import model.SendEmail;
+import Model.SendEmail;
 
 /**
  *
